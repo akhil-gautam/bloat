b66b7e5ca933cc87c1f6823df81febaf7571f298
Akhil Gautam <akhilgautam123@gmail.com>
2026-09-12 23:31:36 +0530

    chore(bloatmac): update 1.1.1 cask

--- Casks/bloatmac.rb
+++ Casks/bloatmac.rb
@@ -1,6 +1,6 @@
 cask "bloatmac" do
-  version "0.1.1"
-  sha256 "ac6e622ad23337fbed7c17d2d1b0df1ebf85785c0ceceecfa3ebe5e991892f28"
+  version "1.1.1"
+  sha256 "3ac5795eb3424dd132094755c94478678c2edcdfbd6b2d6e89e946e2ff71fcc5"
 
   url "https://github.com/akhil-gautam/bloat/releases/download/bloatmac-v#{version}/BloatMac-v#{version}-macos.dmg"
   name "BloatMac"
